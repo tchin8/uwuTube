7dad8d42c0d70fb9481b029bf4118b8a16d76aa6
Tiffany <chin.tiffany108@gmail.com>
2020-09-02 11:12:32 -0400

    frontend auth

--- frontend/uwutube.jsx
+++ frontend/uwutube.jsx
@@ -1,7 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import configureStore from "./store/store";
+import Root from "./components/root";
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
-  ReactDOM.render(<h1>Welcome to uwuTube</h1>, root);
+
+  let preloadedState = {};
+  if (window.currentUser) {
+
+    preloadedState = {
+      session: {
+        id: window.currentUser.id
+      },
+      entities: {
+        users: {
+          [window.currentUser.id]: window.currentUser
+        }
+      }
+    };
+  }
+  const store = configureStore(preloadedState);
+
+  // // TESTING START
+  // window.getState = store.getState;
+  // window.dispatch = store.dispatch;
+  // // TESTING END
+
+  ReactDOM.render(<Root store={store} />, root);
 });
