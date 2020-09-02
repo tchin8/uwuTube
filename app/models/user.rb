68e251002b7ee56606bfcc71194d0b30a3bae801
jlin96 <Jesse13234@gmail.com>
2020-09-02 13:32:13 -0400

    fixed validates typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     validates :fname, :lname, :email, :password_digest, :session_token, presence: true
-    vadlidates :email, :session_token, uniqueness: true
+    validates :email, :session_token, uniqueness: true
     validates :password, length: {minimum: 6}, allow_nil: true
     attr_reader :password
 
