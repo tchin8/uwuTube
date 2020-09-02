950a4016fa884994d10dfb92e74bbc50e41224c4
jlin96 <Jesse13234@gmail.com>
2020-09-02 13:24:14 -0400

    backend user auth

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,3 +1,24 @@
 class Api::SessionsController < ApplicationController
-  
+    skip_before_action :verify_authenticity_token
+
+    def create
+        @user = User.find_by_credentials(params[:user][:email], params[:user][:password])
+        
+        if @user.is_a? Array
+            render json: @user, status: 422
+        else
+            login(@user)
+            render 'api/users/show'
+        end
+    end
+
+    def destroy
+        @user = current_user
+
+        if @user
+            logout!
+        else
+            render json: ['No user to log out'], status: 422
+        end
+    end
 end
