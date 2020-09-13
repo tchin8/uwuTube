765651d3840dc4112dd1cbdb1437b182b70e4d48
Tiffany <chin.tiffany108@gmail.com>
2020-09-12 21:38:09 -0400

    add open uri to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
