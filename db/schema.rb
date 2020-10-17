80a457b8086221e9aec44da82bfe3504d73fc18c
Tiffany <chin.tiffany108@gmail.com>
2020-10-17 13:28:28 -0400

    remove null constraint, user_id for views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_17_171009) do
+ActiveRecord::Schema.define(version: 2020_10_17_172634) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,7 +80,7 @@ ActiveRecord::Schema.define(version: 2020_10_17_171009) do
   end
 
   create_table "views", force: :cascade do |t|
-    t.integer "user_id", null: false
+    t.integer "user_id"
     t.integer "video_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
